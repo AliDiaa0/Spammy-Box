bc4d80a71a559426b9f203b2eb939a6eca047242
AliDiaa0 <85357930+AliDiaa0@users.noreply.github.com>
2022-08-16 20:30:11 +0300

    Add files via upload

---
+++ MSGBOX_Spam/Spamming_Class.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using System.Threading;
+
+namespace MSGBOX_Spam
+{
+    class Spamming_Class
+    {
+        static void Main()
+        {
+            Spamming_Class sc = new Spamming_Class();
+            Thread thread = new Thread(sc.MSGBOXLAUNCH);
+            thread.Start();
+        }
+        public void MSGBOX()
+        {
+            MessageBox.Show("Hey, I'm spamming you, so go subscribe to Ali Diaa's YouTube channel! <3", "Spammy Box", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        public void MSGBOXLAUNCH()
+        {
+            while (true)
+            {
+                Thread MSGBOXT = new Thread(MSGBOX);
+                MSGBOXT.Start();
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
